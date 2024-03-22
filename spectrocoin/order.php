520416f111ac0be2be081c598c7ebc16672ffd3e
Rokas Reizgys <rokas.student@gmail.com>
2024-03-22 13:23:15 +0200

    Migrating to new api

--- spectrocoin/order.php
+++ spectrocoin/order.php
@@ -4,16 +4,14 @@ include '../../../includes/functions.php';
 include '../../../includes/gatewayfunctions.php';
 include '../../../includes/invoicefunctions.php';
 require_once 'lib/SCMerchantClient/SCMerchantClient.php';
-$gatewaymodule = "spectrocoin";
-$GATEWAY = getGatewayVariables($gatewaymodule);
-// get invoice
-$invoiceId = (int)$_POST['invoiceId'];
+$GATEWAY = getGatewayVariables("spectrocoin");
+$invoice_id = (int)$_POST['invoiceId'];
 $price = $currency = false;
-$result = mysql_query("SELECT tblinvoices.total, tblinvoices.status, tblcurrencies.code FROM tblinvoices, tblclients, tblcurrencies where tblinvoices.userid = tblclients.id and tblclients.currency = tblcurrencies.id and tblinvoices.id=$invoiceId");
+$result = mysql_query("SELECT tblinvoices.total, tblinvoices.status, tblcurrencies.code FROM tblinvoices, tblclients, tblcurrencies where tblinvoices.userid = tblclients.id and tblclients.currency = tblcurrencies.id and tblinvoices.id=$invoice_id");
 $data = mysql_fetch_assoc($result);
 
 if (!$data) {
-    error_log('No invoice found for invoice id' . $invoiceId);
+    error_log('No invoice found for invoice id' . $invoice_id);
     die("Invalid invoice");
 }
 
@@ -32,7 +30,7 @@ $project_id = $GATEWAY['projectId'];
 $client_id = $GATEWAY['clientId'];
 $client_secret = $GATEWAY['clientSecret'];
 
-if (!$privateKey || !$userId || !$projectId)
+if (!$project_id || !$client_id || !$client_secret)
 {
     echo 'SpectroCoin is not fully configured. Please select different payment';
     exit;
@@ -43,22 +41,22 @@ if ($amount < 0) {
     exit;
 }
 
+$order_description = "Order #{$invoice_id}";
+$callback_url = $options['systemURL'] . 'modules/gateways/callback/spectrocoin.php?invoice_id=' . $invoice_id;
+$success_url = $options['systemURL'] . '';
+$cancel_url = $options['systemURL'] . 'modules/gateways/callback/spectrocoin.php?cancel&invoice_id=' . $invoice_id;
+
 $auth_url = "https://test.spectrocoin.com/api/public/oauth/token";
 $api_url = 'https://spectrocoin.com/api/merchant/1';
 
-$orderDescription = "Order #{$invoiceId}";
-$callbackUrl = $options['systemURL'] . 'modules/gateways/callback/spectrocoin.php?invoice_id=' . $invoiceId;
-$successUrl = $options['systemURL'] . '';
-$cancelUrl = $options['systemURL'] . 'modules/gateways/callback/spectrocoin.php?cancel&invoice_id=' . $invoiceId;
-
-$client = new SCMerchantClient($merchantApiUrl, $userId, $projectId);
-$orderRequest = new Spectrocoin_CreateOrderRequest(null, "BTC", null, $currency, $amount, $orderDescription, "en", $callbackUrl, $successUrl, $cancelUrl);
-$response =$client->spectrocoinCreateOrder($orderRequest);
+$client = new SCMerchantClient($api_url, $project_id, $client_id, $client_secret, $auth_url);
+$order_request = new Spectrocoin_CreateOrderRequest(null, "BTC", null, $currency, $amount, $order_description, "en", $callback_url, $success_url, $cancel_url);
+$response =$client->spectrocoinCreateOrder($order_request);
 if ($response instanceof Spectrocoin_ApiError) {
     error_log('Error getting response from SpectroCoin. Error code: ' . $response->getCode() . ' Error message: ' . $response->getMessage());
     echo 'Error getting response from SpectroCoin. Error code: ' . $response->getCode() . ' Error message: ' . $response->getMessage();
     exit;
 } else {
-	$redirectUrl = $response->getRedirectUrl();
-	header('Location: ' . $redirectUrl);
+	$redirect_url = $response->getRedirectUrl();
+	header('Location: ' . $redirect_url);
 }
